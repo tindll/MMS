327216c6eb80d97993f64195238d6df21e30eaae
tin.dll <zjamsty@gmail.com>
2021-05-02 20:58:32 +0200

    added lots of stuff

--- testAPI.py
+++ testAPI.py
@@ -4,20 +4,21 @@ import pandas as pd
 import numpy as np
 import datetime
 from binance.client import Client
+from finta import TA
 
 ## setting up binance API from environment variables ##
 api_key = os.environ.get('API_KEY')
 api_secret = os.environ.get('API_SECRET')
 client = Client(api_key, api_secret)
-#client.API_URL = 'https://testnet.binance.vision/api'
+
 # get balances for all assets & some account information
 #print(client.get_account())
 # get balance for a specific asset only (BTC)
 #print(client.get_asset_balance(asset='BTC'))
+
 # get balances for futures account
 print(client.futures_account_balance())
-# get balances for margin account
-#print(client.get_margin_account())
+
 # get latest price from Binance API
 btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
 # print full output (dictionary)
@@ -26,12 +27,65 @@ print(btc_price)
 #{'symbol': 'BTCUSDT', 'price': '9678.08000000'}
 #We can access just the price as follows.
 #print(btc_price["price"])
+def create_plot(df):
+    mc = mpf.make_marketcolors(up='w',down='b')
+    s  = mpf.make_mpf_style(marketcolors=mc)
 
-#candles = client.get_klines(symbol='BNBBTC', interval=Client.KLINE_INTERVAL_30MINUTE)
-#print(candles)
-klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "7 day ago UTC")
-print(klines)
-#tickers = client.get_orderbook_tickers()
-for kline in client.get_historical_klines_generator("BNBBTC", Client.KLINE_INTERVAL_15MINUTE, "1 day ago UTC"):
-    print(kline)
-    # do something with the kline
+    ap0 = [ mpf.make_addplot(df['BB_UPPER'],color='cyan'), #BBANDS
+            mpf.make_addplot(df['BB_LOWER'],color='cyan'), #BBANDS
+            mpf.make_addplot(df['BB_MIDDLE'],color='grey'), #BBANDS
+            mpf.make_addplot(df["RSI"], panel='lower', color='purple'),
+            mpf.make_addplot(df["MACD"], panel='lower', color='red'),
+            mpf.make_addplot(df["MACD_signal"], panel='lower', color='orange')
+          ]
+    MACD = mpf.make_addplot(df["MACD"], panel='lower')
+    MACD_signal= mpf.make_addplot(df["MACD_signal"], panel='lower')
+    RSI = mpf.make_addplot(df["RSI"], panel='lower')
+
+    mpf.plot(df, type='candle', axtitle = "BTCUSDT 1D", xrotation=20, datetime_format=' %A, %d-%m-%Y', savefig='chart.png', volume = True, volume_panel=2, style = s,addplot=ap0, fill_between=dict(y1=df['BB_LOWER'].values, y2=df['BB_UPPER'].values, alpha=0.15))
+    #mpf.plot(df, type='candle', style='yahoo', addplot=MACDplots, ylabel='', ylabel_lower='')
+    #mpf.plot(df,closefig=True)
+
+def valuesforDF():
+    #fills dataframe with information : open, close, etc... & rsi, macd, bbands
+    open,high,low,close,time,pandasdti,volume = [],[],[],[],[],[],[]
+    for kline in client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "90 day ago UTC"):
+        pandasdti.append(pd.to_datetime((datetime.datetime.fromtimestamp(kline[0]/1000).strftime('%Y-%m-%d %H:%M'))))
+        open.append(float(kline[1]))
+        high.append(float(kline[2]))
+        low.append(float(kline[3]))
+        close.append(float(kline[4]))
+        volume.append(float(kline[5]))
+
+    zippedList = list(zip(pandasdti, open, high, low, close))
+    df = pd.DataFrame(zippedList, columns = ['datetime', 'open' , 'high', 'low', 'close'])
+    df = df.set_index(['datetime'])
+    df['volume'] = volume
+
+    bband = TA.BBANDS(df)
+    df['BB_MIDDLE'] = bband['BB_MIDDLE']
+    df['BB_UPPER'] = bband['BB_UPPER']
+    df['BB_LOWER'] = bband['BB_LOWER']
+
+    bbwidth = TA.BBWIDTH(df)
+    df['BBWIDTH'] = bbwidth
+
+    macd = TA.MACD(df)
+    df['MACD'] = macd['MACD']
+    df['MACD_signal'] = macd['SIGNAL']
+
+    RSI= TA.RSI(df)
+    df['RSI'] = RSI
+
+    create_plot(df)
+    print(df)
+    df.to_csv(r'c:\data\pandas.txt', header=None, index=None, sep='-', mode='a')
+
+def RSIDivergence():
+    #regular bullish divergence : price(lower low or equal low) & osci(higher low)
+    #hidden bullish divergence : price(higher low) & osci(lower low)
+    #regular bearish divergence : price(higher high or equal low) & osci(lower high)
+    #hidden bearish divergence : price(lower high) & osci(higher high)
+
+
+valuesforDF()
