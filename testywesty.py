044bbde05e6dafae77b77dc6dfd7e6d05639b834
tin.dll <zjamsty@gmail.com>
2021-06-10 01:51:38 +0200

    minor tweaks & added testwesty

---
+++ testywesty.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+
+symbols = ["BTCUSDT", "ETHUSDT", "BCHUSDT", "XRPUSDT", "EOSUSDT", "LTCUSDT", "TRXUSDT", "ETCUSDT", "XLMUSDT","LINKUSDT"]
+timeframes = ["15m", "1h", "24h"]
+
+for symbol in symbols:
+    for times in timeframes:
+        if times == "15m":
+            subprocess.run(["python3", "testAPI.py",symbol,times,"2"])
+            time.sleep(3)
+
+        if times == "1h":
+            subprocess.run(["python3", "testAPI.py",symbol,times,"7"])
+            time.sleep(3)
+
+        if times == "1d":
+            subprocess.run(["python3", "testAPI.py",symbol,times,"100"])    
+            time.sleep(3)
