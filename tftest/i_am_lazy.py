bf98a0ea9bb786d515278128d5de796d3f37001b
tin.dll <zjamsty@gmail.com>
2021-06-20 08:58:57 +0200

    i've basically got a 100ma, nothing predictive

--- tftest/i_am_lazy.py
+++ tftest/i_am_lazy.py
@@ -2,3 +2,6 @@ import subprocess
 
 subprocess.run(["docker", "cp","test1.py","intelligent_goldberg:/testingTF/test1.py"])
 subprocess.run(["docker", "exec","-it","intelligent_goldberg", "python3","testingTF/test1.py"])
+subprocess.run(["docker", "cp","intelligent_goldberg:/testingTF/plotty_wotty.png","."])
+subprocess.run(["docker", "cp","intelligent_goldberg:/testingTF/model_plot.png","."])
+#docker cp intelligent_goldberg:/testingTF/plotty_wotty.png .
